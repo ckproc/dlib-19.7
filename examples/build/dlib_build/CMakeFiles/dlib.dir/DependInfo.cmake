0ca40f5e85de2436e557bee9a805d3987d2d9507
ckp <your_email@youremail.com>
2018-04-09 20:42:33 +0800

    first commit

---
+++ examples/build/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ckp/dlib-19.7/dlib/base64/base64_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/base64/base64_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/bigint/bigint_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/bigint/bigint_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/bigint/bigint_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/bigint/bigint_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/bit_stream/bit_stream_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/bit_stream/bit_stream_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/bsp/bsp.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/bsp/bsp.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/data_io/image_dataset_metadata.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/data_io/image_dataset_metadata.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/data_io/mnist.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/data_io/mnist.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dir_nav/dir_nav_extensions.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_extensions.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dir_nav/dir_nav_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dir_nav/dir_nav_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dir_nav/dir_nav_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/cpu_dlib.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/cpu_dlib.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/cublas_dlibapi.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/cublas_dlibapi.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/cuda_data_ptr.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/cuda_data_ptr.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/cudnn_dlibapi.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/cudnn_dlibapi.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/curand_dlibapi.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/curand_dlibapi.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/gpu_data.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/gpu_data.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/dnn/tensor_tools.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/dnn/tensor_tools.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/entropy_decoder/entropy_decoder_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/entropy_decoder/entropy_decoder_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/entropy_decoder/entropy_decoder_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/entropy_decoder/entropy_decoder_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/entropy_encoder/entropy_encoder_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/entropy_encoder/entropy_encoder_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/entropy_encoder/entropy_encoder_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/entropy_encoder/entropy_encoder_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_core/gui_core_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_core/gui_core_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_core/gui_core_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_core/gui_core_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_widgets/base_widgets.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_widgets/base_widgets.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_widgets/canvas_drawing.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_widgets/canvas_drawing.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_widgets/drawable.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_widgets/drawable.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_widgets/fonts.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_widgets/fonts.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_widgets/style.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_widgets/style.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/gui_widgets/widgets.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/gui_widgets/widgets.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/image_loader/jpeg_loader.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/image_loader/jpeg_loader.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/image_loader/png_loader.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/image_loader/png_loader.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/image_saver/save_jpeg.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/image_saver/save_jpeg.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/image_saver/save_png.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/image_saver/save_png.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/linker/linker_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/linker/linker_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/logger/extra_logger_headers.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/logger/extra_logger_headers.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/logger/logger_config_file.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/logger/logger_config_file.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/logger/logger_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/logger/logger_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/md5/md5_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/md5/md5_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/misc_api/misc_api_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/misc_api/misc_api_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/misc_api/misc_api_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/misc_api/misc_api_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/server/server_http.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/server/server_http.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/server/server_iostream.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/server/server_iostream.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/server/server_kernel.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/server/server_kernel.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/sockets/sockets_extensions.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_extensions.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/sockets/sockets_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/sockets/sockets_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/sockets/sockets_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/sockstreambuf/sockstreambuf.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/sockstreambuf/sockstreambuf.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/sockstreambuf/sockstreambuf_unbuffered.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/sockstreambuf/sockstreambuf_unbuffered.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/stack_trace.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/stack_trace.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/async.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/async.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/multithreaded_object_extension.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/multithreaded_object_extension.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/thread_pool_extension.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/thread_pool_extension.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/threaded_object_extension.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/threaded_object_extension.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/threads_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/threads_kernel_2.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_2.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/threads/threads_kernel_shared.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/threads/threads_kernel_shared.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/timer/timer.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/timer/timer.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/tokenizer/tokenizer_kernel_1.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/tokenizer/tokenizer_kernel_1.cpp.o"
+  "/home/ckp/dlib-19.7/dlib/unicode/unicode.cpp" "/home/ckp/dlib-19.7/examples/build/dlib_build/CMakeFiles/dlib.dir/unicode/unicode.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
